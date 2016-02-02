a977b15ab5d7a2273a0d299e0a813b46aa0ef566
ajayvahan <ajay.vahan@mindfiresolutions.com>
2016-02-02 20:02:05 +0530

    added ajax request to login page

--- cgi-bin/app/login.py
+++ cgi-bin/app/login.py
@@ -4,10 +4,10 @@
 import MySQLdb
 import cgi, cgitb 
 import smtplib
-
+import json
 # Create instance of FieldStorage 
 form = cgi.FieldStorage() 
-
+cgitb.enable()
 db = MySQLdb.connect("localhost","root","mindfire","assignment")
 
 # setup a cursor object using cursor() method
@@ -19,51 +19,57 @@ login_password = form.getvalue('password')
 
 print "Content-type:text/html\r\n\r\n"
 print
-
+response = dict()
 
 try:
 
-	db1 = "SELECT pk_id FROM user_detail where username = '{0}' and password = '{1}'".format(login_username,login_password)
-	cursor.execute(db1)
+    db1 = "SELECT pk_id FROM user_detail where username = '{0}' and password = '{1}'".format(login_username,login_password)
+    cursor.execute(db1)
 
-	if cursor.fetchone():
+    if cursor.fetchone():
 
-		db2="SELECT pk_id FROM user_detail where status='active' and username='{0}'".format(login_username)
-		cursor.execute(db2)
-		if cursor.fetchone():
-			db3="SELECT first_name,last_name,username,gender,dob,email,mobile,pk_id,image FROM user_detail where username='{0}'".format(login_username)
-			cursor.execute(db3)
+        db2="SELECT pk_id FROM user_detail where status='active' and username='{0}'".format(login_username)
+        cursor.execute(db2)
+        if cursor.fetchone():
+            db3="SELECT first_name,last_name,username,gender,dob,email,mobile,pk_id,image FROM user_detail where username='{0}'".format(login_username)
+            cursor.execute(db3)
 
-			for row in cursor.fetchall():
-				first_name=row[0]
-				last_name=row[1]
-				username=row[2]
-				gender=row[3]
-				dob=row[4]
-				email=row[5]
-				mobile=row[6]
-				pk_id=row[7]
-				image=row[8]
-		
+            for row in cursor.fetchall():
+                first_name=row[0]
+                last_name=row[1]
+                username=row[2]
+                gender=row[3]
+                dob=row[4]
+                email=row[5]
+                mobile=row[6]
+                pk_id=row[7]
+                image=row[8]
+        
+            response.update({'success': False, 'message': "logging in", 'status':'active'})            
+            print json.JSONEncoder().encode(response)
+            fh=open('../../static/html_data/profile.txt')
+            for line in fh:
+                print ''.join(line).format(first_name,last_name,username,gender,dob,email,mobile,pk_id,image)
+        else:
+            # fh=open('../../static/html_data/login_message.txt')
+            # for line in fh:
+            #   print ''.join(line).format("Your account is not active")
+            response.update({'success': True, 'message': "Your account is not active", 'status':'not active'})            
+            print json.JSONEncoder().encode(response)
+            
+    else:
+        # fh=open('../../static/html_data/login_error.txt')
+        # for line in fh:
+        #     print ''.join(line)
+        response.update({'success': True, 'message': 'wrong username or password', 'status':'error'})
+        print json.JSONEncoder().encode(response)
 
-			fh=open('../../static/html_data/profile.txt')
-			for line in fh:
-				print ''.join(line).format(first_name,last_name,username,gender,dob,email,mobile,pk_id,image)
-		else:
-			fh=open('../../static/html_data/login_message.txt')
-			for line in fh:
-				print ''.join(line).format("Your account is not active")
-			
-	else:
-		fh=open('../../static/html_data/login_error.txt')
-		for line in fh:
-			print ''.join(line)
-			
-		
 except Exception as e:
-	fh=open('../../static/html_data/login_message.txt')
-	for line in fh:
-		print ''.join(line).format(e)
+    # fh=open('../../static/html_data/login_message.txt')
+    # for line in fh:
+    #     print ''.join(line).format(e)
+    response.update({'success': True, 'message': e, 'status': 'exception'})
+    print json.JSONEncoder().encode(response)
 
 
 # close the mysql database connection
--- static/js/login_validate_jquery.js
+++ static/js/login_validate_jquery.js
@@ -1,7 +1,7 @@
 function error_call(element_id,txt){
 	// $(element_id).parent().removeClass("has-feedback");
 	$(element_id).parent().addClass("has-error");
-	$(element_id).next().removeClass("glyphicon glyphicon-user glyphicon-lock form-control-feedback");
+	$(element_id).next().removeClass("glyphicon glyphicon-user glyphicon-eye-open form-control-feedback");
 	$(element_id).next().addClass("glyphicon glyphicon-remove form-control-feedback");
 	$(element_id).next().next().html('<div class="text-danger">'+txt +'</div>');
 }
@@ -9,35 +9,93 @@ function error_call(element_id,txt){
 
 $(document).ready(function(){
 	$("input").keypress(function(){
-		$(this).parent().removeClass("has-error has-feedback");
-		$(this).next().removeClass("glyphicon glyphicon-remove form-control-feedback");
+		$(this).parent().removeClass("has-error");
+		$(this).next().removeClass("glyphicon-remove");
+		$("#username").next().addClass("glyphicon-user");
+		$("#password").next().addClass("glyphicon-eye-open");
 		$(this).next().next().html('');
 
 	});
 	
-	$("#submit").on("click", function(){
+	$("#login_submit").on("click", function(e){
+		e.preventDefault();
 		var username= $("#username").val();
 		var password = $("#password").val();
-		var error_flag=1;
+		var error_flag=0;
 		
 
 		if(username =="" || username == "None"){
 			error_call('#username','please fill username field');
-			error_flag = 0;
+			error_flag = 1;
 		}
 
 		if(password == "" || password == "None"){
 			error_call("#password",'please fill password field')
-			error_flag = 0; 
+			error_flag = 1; 
 
 
 		}
 
 		if(error_flag){
-			return true;
+			return false;
 		}
 		else{
-			return false;
+			$.ajax({
+				type: "POST",
+				dataType: "json",
+				url: "/cgi-bin/app/login.py",
+				data : $("#myForm").serialize(),
+
+				success: function(data) {
+					console.log(data);
+					console.log(data['success']);
+					if(data['success']){
+						if(data['status'] == 'not active'){
+							$("#error_message").addClass("alert alert-danger")
+							$("#error_message").children().html('<strong>Error!'+data['message']+'</strong>');
+							$("#username").parent().addClass("has-error");
+							$("#username").next().removeClass("glyphicon glyphicon-user glyphicon-eye-open form-control-feedback");
+							$("#username").next().addClass("glyphicon glyphicon-remove form-control-feedback");
+							$('#username').next().next().html('');
+							$("#password").parent().addClass("has-error");
+							$("#password").next().removeClass("glyphicon glyphicon-user glyphicon-eye-open form-control-feedback");
+							$("#password").next().addClass("glyphicon glyphicon-remove form-control-feedback");
+							$('#password').next().next().html('');
+
+							error_flag = 1;
+							return false;	
+						}
+						else if(data['status'] == 'error'){
+							alert(data['message']);
+							error_call("#username","wrong username or password");
+							error_call("#password","wrong username or password");
+							error_flag = 1;
+							return false;
+							
+						}
+						else if (data['status'] == 'exception'){
+							$("#error_message").addClass("alert alert-danger")
+							$("#error_message").children().html('<strong>Error!'+data['message']+'</strong>');
+							$("#username").parent().addClass("has-error");
+							$("#username").next().removeClass("glyphicon glyphicon-user glyphicon-eye-open form-control-feedback");
+							$("#username").next().addClass("glyphicon glyphicon-remove form-control-feedback");
+							$('#username').next().next().html('');
+							$("#password").parent().addClass("has-error");
+							$("#password").next().removeClass("glyphicon glyphicon-user glyphicon-eye-open form-control-feedback");
+							$("#password").next().addClass("glyphicon glyphicon-remove form-control-feedback");
+							$('#password').next().next().html('');
+
+							error_flag = 1;
+							return false;
+						}
+						
+					}
+				},
+				error : function(data){
+					$("#myForm").submit();		
+				}	
+
+			});
 		}
 
 	});
